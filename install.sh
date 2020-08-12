dc53a666f9d7a925c0938cb2ff45cc51653363ff
Fitzy1293 <42453847+Fitzy1293@users.noreply.github.com>
2020-08-12 16:38:52 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 cd ~ && pip3 install redditcleaner && mkdir -p redditsfinder && cd redditsfinder && wget https://raw.githubusercontent.com/Fitzy1293/redditsfinder/master/redditsfinder.py
-mkdir -p ~/bin && cp push.py ~/bin/redditsfinder && cd ~/bin && chmod +x redditsfinder && rm -rf ~/redditsfinder && cd ~ 
+mkdir -p ~/bin && cp redditsfinder.py ~/bin/redditsfinder && cd ~/bin && chmod +x redditsfinder && rm -rf ~/redditsfinder && cd ~ 
 echo "Successfully installed redditsfinder"
