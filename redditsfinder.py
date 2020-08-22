065c8708a770a729dbc50d4503226178793e2587
Fitzy1293 <42453847+Fitzy1293@users.noreply.github.com>
2020-08-22 00:45:10 -0400

    Add files via upload

--- redditsfinder.py
+++ redditsfinder.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-import urllib.request
+
+
+'''
+github.com/fitzy1293/redditsfinder
+The README.md is helpful
+
+'''
+
+import urllib.request, requests
 import json
 import time
-import os
+import os,sys
 from pprint import pprint
-import sys
 from urllib.error import HTTPError
 from datetime import datetime
+import imghdr
+
 import redditcleaner
-
-# f strings are amazing.
-
 
 # Makes body and selftext not an abomination.
 def humanReadablePost(redditRawText):
@@ -33,9 +39,10 @@ def humanReadablePost(redditRawText):
     # 1D list with each item containing a max of 15 words.
     return [' '.join(cleanPost) for cleanPost in splitWords]
 
-
-# Uses pushshift API. Functions kind of a mess but works.
-def getPosts(user, keyType):
+#─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+#=============================================================================================================================
+#─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+def getPosts(user, keyType, picFlag): # Uses pushshift API. Functions kind of a mess but works.
 
     apiUrl = 'https://api.pushshift.io/reddit/search/'
     # Max num of posts in each pushshift request, seems to be 100 right now or it breaks.
@@ -46,18 +53,21 @@ def getPosts(user, keyType):
     beginTime = before  # To reset time to original value after comments.
     allPosts = {}
 
-    # This for loop is because a lot of the code would have been the same for comments and submissions.
-    # Things like if key == 'body' or key == 'selftext': can deal with a problem both comments and some submissions have.
-    for postType in ('comment', 'submission'):
-        print()
+
+    if picFlag:
+        correctPostType = ['submission']
+    else:
+        correctPostType = ['comment', 'submission']
+
+
+    for postType in correctPostType:
 
         print(f'{postType[0].upper()}{postType[1:]} request log:')
 
-        ct = 0  # For logging each batch of posts returned by pushshift.
+        ct = 0
         posts = []
         while True:  # We need to wait until we've collected all posts then break.
             time.sleep(.75)  # Avoids rate limits.
-            # API request varying before while the while loop isn't broken.
             url = f'{apiUrl}{postType}/?author={user}&size={postSetMaxLen}&before={before}'
             try:
                 response = urllib.request.urlopen(url)
@@ -66,20 +76,18 @@ def getPosts(user, keyType):
                 for post in data:
                     # Relevant keys for either a comment or submission.
                     ourKeys = keyType[postType]
-                    # All pushshift keys for this post, we do not want them all - we need readable output.
-                    apiKeys = post.keys()
 
-                    # Kinda forgot why this is written like this.
+
                     postDict = dict.fromkeys(ourKeys, None)
 
                     for key in ourKeys:  # We are doing things for specific keys, so we need another loop.
-                        if key in ourKeys and key in apiKeys:  # To only use the keys we need.
-
+                        if key in ourKeys and key in post.keys():  # To only use the keys we need.
                             outputValue = post[key]
 
                             # Thanks redditcleaner making this less painful.
                             if key == 'body' or key == 'selftext':
                                 outputValue = humanReadablePost(outputValue)
+
 
                             # Create a datetime object from timestamp.
                             if key == 'created_utc':
@@ -89,8 +97,6 @@ def getPosts(user, keyType):
 
                             postDict[key] = outputValue
 
-                    postDict['postType'] = postType
-
                     posts.append(postDict)
 
                 # Cause if it is there's something there which means more posts to get.
@@ -99,24 +105,48 @@ def getPosts(user, keyType):
                     before = posts[-1]['created_utc']
 
                 ct = ct + len(data)
-                # print(f'\t{ct+1} - {len(data)} {url}') #Log for each API request.
                 print(f'\t{ct} {url}')
 
                 if len(data) < postSetMaxLen:  # Get 100 posts at a time they switched from 1000?
                     allPosts[postType + 's'] = posts
-                    break
 
-                # ct+=1
+                    break
 
             except HTTPError:  # The sleep .75 deals with this.
                 print('Rate limited')
 
         # before has been decreasing and we don't want it to start at the last comment for the beggining of submissions.
         before = beginTime
+        print()
 
-    return allPosts
+    if picFlag:
+        urls = [v for i in allPosts['submissions']  for k,v in i.items() if k == 'url']
+        imageUrls = []
+        for url in urls:
+            if url.endswith(('.png', '.PNG', '.jpg','.JPG')):
+                imageUrls.append(url)
+                continue
 
+            if '://reddit.com' in url:
+                continue
+            if 'imgur.com' in url:
+                if '/a/' in url:
+                    imageUrls.append(url + '/zip')
+                else:
+                    imageUrls.append(url + '.jpg')
 
+        imageUrlsStr = '\n'.join(imageUrls)
+        images = os.path.join(os.getcwd(), 'users', user, 'images.txt')
+        with open(images, 'w+') as f:
+            f.write(imageUrlsStr+ '\n')
+        return imageUrlsStr
+
+    else:
+        return allPosts
+
+#─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+#=============================================================================================================================
+#─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 def countPosts(allPosts):  # Count and order most posted subs.
     postCounts = {}
     for postType, posts in allPosts.items():
@@ -134,21 +164,15 @@ def countPosts(allPosts):  # Count and order most posted subs.
 
     return postCounts
 
+#─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+#=============================================================================================================================
+#─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 
-def writeFiles(allPosts, postCounts, user):
-
-    # New folder containing a folder for each username.
-    usersDir = os.path.join(os.getcwd(), 'users')
-    userDir = os.path.join(usersDir, user)  # Contains files for username.
-    for dir in (usersDir, userDir):
-        if not os.path.exists(dir):
-            os.mkdir(dir)
-
+def writeFiles(allPosts, postCounts, user, userDir):
     if len(allPosts) != 0:
         jPath = os.path.join(userDir, 'all_posts.json')
         with open(jPath, 'w+', newline='\n') as f:
             json.dump(allPosts, f, indent=4)
-
 
         tPath = os.path.join(userDir, 'subreddit_count.txt')
         with open(tPath, 'w+') as g:
@@ -157,22 +181,26 @@ def writeFiles(allPosts, postCounts, user):
                 for i in v:
                     g.write(f'{i[0]}, {str(i[1])}\n')
 
+#─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+#=============================================================================================================================
+#─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+
 def makeBox(list): #Takes 1 d list of strings, returns str in a box that fits.
     longestStr = len(max(list, key=len))
-    box = str('─') * (longestStr + 1)
+    box = str('─') * (longestStr + 1) #Box drawing char, not dash.
 
-    boxedStr = '\n'.join([f'│{i}{ str(" " * ( longestStr + 1 - len(i) )) }│' for i in list])
+    boxedStr = '\n'.join([f'│{i}{ str(" " * (longestStr + 1 - len(i))) }│' for i in list])
 
     return f'┌{box}┐\n{boxedStr}\n└{box}┘' #(U+2518) and (U+2510) box drawing chars respecitvely.
 
-
+#─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+#=============================================================================================================================
+#─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 
 def printTotals(totalsDict): #Printed stuff after the pushshift log.
-    print()
-    countableTab = str(" " * 4) #For getting length of string to box easier because a tab is only one character.
 
-    ##############################################################################################################################
-    sortedPrintList = []
+    countableTab = ' ' * 4 #For getting length of string to box because a tab is only one character.
+
     for k, v in totalsDict['postCounts'].items():
         postTypeList = [k]
         for subreddit in v:
@@ -180,15 +208,9 @@ def printTotals(totalsDict): #Printed stuff after the pushshift log.
             new = f'{countableTab}{subreddit[0]} {subreddit[1]}'
             postTypeList.append(new)
 
-        #postTypeList.append('')
-
         print(makeBox(postTypeList))
 
-    #*****************************************************************************************************************************
-
-    ##############################################################################################################################
     #Box containing # of comments/submissions, dir and path of files, and run time.
-
     comments = f'Comments{countableTab}= {totalsDict["commentsLenPrint"]}'
     submissions = f'Submissions = {totalsDict["submissionsLenPrint"]}'
     dir = totalsDict['dir']
@@ -201,50 +223,96 @@ def printTotals(totalsDict): #Printed stuff after the pushshift log.
 
     print(makeBox(infoToBox))
 
-    #*****************************************************************************************************************************
-
-
-
-
-def run(user):
+#─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+#=============================================================================================================================
+#─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+def run(user, options):
     start = time.time()
 
-    # Pushshift attributes I thought were useful.
-    keyType = {'comment': ('id', 'created_utc', 'subreddit', 'body', 'score', 'permalink', 'link_id', 'parent_id'),
-               'submission': ('id', 'created_utc', 'subreddit', 'selftext', 'score', 'full_link', 'url')}
-
-    print(f'**Gathering and formatting data from reddit user {user}**')
-
-    allPosts = getPosts(user, keyType)
+    usersDir = os.path.join(os.getcwd(), 'users')
+    userDir = os.path.join(usersDir, user)  # Contains files for username.
+    for dir in (usersDir, userDir):
+        if not os.path.exists(dir):
+            os.mkdir(dir)
 
 
-    postCounts = countPosts(allPosts)
+    print(f'Gathering and formatting data from pushshift for {user}.\n')
 
-    writeFiles(allPosts, postCounts, user)
+    if '-pics' in options:
+        keyType = {'submission': ('url', 'created_utc',)}
+        images = getPosts(user, keyType, True)
 
-    #printTotals makes the terminal output look nice
-    totalsDict = {'postCounts': postCounts,
-                  'commentsLenPrint': str( len(allPosts['comments']) ),
-                  'submissionsLenPrint': str( len(allPosts['submissions']) ),
-                  'dir': os.path.join(os.getcwd(), 'users', user),
-                  'start': start,
-                  'end': time.time()}
+        images = open( os.path.join(userDir,'images.txt')).read().splitlines()
+        for i, image in enumerate(images):
+            print(f'{i+1}\t{image}')
 
-    printTotals(totalsDict)
+        if '-d' in options:
+            print()
+
+            for i, url in enumerate(images):
+
+                response = requests.get(url, timeout=10)
+
+                if url.endswith(('.png', '.PNG', '.jpg','.JPG')):
+                    fileType = f'.{url.split(".")[-1]}'
+                    imagePath = os.path.join(userDir, f'{str(i+1)}{fileType}')
+                    with open(imagePath, 'wb+') as f:
+                        f.write(response.content)
+
+                    imghdrExtension = imghdr.what(imagePath) #Makes some jpg None?
+                    if str(imghdrExtension) == 'None':
+                        imghdrExtension = 'jpeg'
+
+                    realFileType = f'{"".join(imagePath.split(".")[0:-1])}.{imghdrExtension}' #Checks filetype not by extension.
+                    newFname = os.path.join(userDir, realFileType)
+                    os.rename(imagePath, newFname)
+                    imagePath = newFname
+
+                else:
+
+                    imagePath = os.path.join(userDir, f'{i+1}.zip')
+                    with open(imagePath, 'wb+') as f:
+                        f.write(response.content)
+
+                print(f'Downloaded {os.path.split(imagePath)[-1]}\t{url}' )
 
 
+    else:
+        keyType = {'comment': ('id', 'created_utc', 'subreddit', 'body', 'score', 'permalink', 'link_id', 'parent_id'),
+                   'submission': ('id', 'created_utc', 'subreddit', 'selftext', 'score', 'full_link', 'url')}
+
+        allPosts = getPosts(user, keyType, False)
+        postCounts = countPosts(allPosts)
+        writeFiles(allPosts, postCounts, user, userDir)
 
 
-if __name__ == '__main__':  # System arguments
+        totalsDict = {'postCounts': postCounts,
+                      'commentsLenPrint': str( len(allPosts['comments']) ),
+                      'submissionsLenPrint': str( len(allPosts['submissions']) ),
+                      'dir': userDir,
+                      'start': start,
+                      'end': time.time()}
+
+        printTotals(totalsDict)
+
+#─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+#=============================================================================================================================
+#─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+if __name__ == '__main__':  # System arguments.
+
     if len(sys.argv) == 1:
         print('Remember to add a username')
         print('For help enter redditsfinder -h')
+
     elif len(sys.argv) == 2:
+
         if sys.argv[-1] == '-h':
             print('Only argument is a username')
             print('Adding more options soon')
         else:
-            run(sys.argv[1])
+            run(sys.argv[-1], [''])
 
-    elif len(sys.argv):
-        print('Too many arguments')
+
+    elif len(sys.argv) >= 3:
+        optArgs = [arg for arg in sys.argv[1:-1]]
+        run(sys.argv[-1], optArgs)
