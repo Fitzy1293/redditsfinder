ac99ac2817d3059e322edd3502255870e7c15835
Fitzy1293 <42453847+Fitzy1293@users.noreply.github.com>
2020-09-20 09:50:23 -0400

    Add files via upload
    MAJOR CHANGES TO FILE TO HOW IT'S DISPLAYED IN THE TERMINAL

--- redditsfinder.py
+++ redditsfinder.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 '''
 github.com/fitzy1293/redditsfinder
@@ -14,6 +14,10 @@ from urllib.error import HTTPError
 from datetime import datetime
 import imghdr
 from zipfile import ZipFile
+
+from rich.table import Table,Column
+from rich.console import Console
+from rich.panel import Panel
 
 import redditcleaner #Not in standard lib.
 
@@ -41,6 +45,7 @@ def humanReadablePost(redditRawText): # Makes body and selftext not an abominati
 #=============================================================================================================================
 #─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 def getPosts(user, keyType, picFlag): # Uses pushshift API. Functions kind of a mess but works.
+    console = Console()
 
     apiUrl = 'https://api.pushshift.io/reddit/search/'
     # Max num of posts in each pushshift request, seems to be 100 right now or it breaks.
@@ -58,7 +63,11 @@ def getPosts(user, keyType, picFlag): # Uses pushshift API. Functions kind of a 
 
     for postType in correctPostType:
 
-        print(f'{postType[0].upper()}{postType[1:]} request log:')
+        console.print(f'[bold blue]{postType[0].upper()}{postType[1:]} request log:')
+        if postType == 'comment':
+            highlight = '[cyan]'
+        else:
+            highlight = '[magenta]'
 
         ct = 0
         posts = []
@@ -97,7 +106,7 @@ def getPosts(user, keyType, picFlag): # Uses pushshift API. Functions kind of a 
                     before = posts[-1]['created_utc']
 
                 ct = ct + len(data)
-                print(f'\t{ct} {url}')
+                console.print(f'{highlight}\t{ct} {url}')
 
                 if len(data) < postSetMaxLen:  # Get 100 posts at a time they switched from 1000?
                     allPosts[postType + 's'] = posts
@@ -150,7 +159,7 @@ def countPosts(allPosts):  # Count and order most posted subs.
 
         #Sort by number of posts user has in each sub that they have posted in.
         sortedCounts = sorted(counts, key=lambda subredditPostCount: (subredditPostCount[1], subredditPostCount[0]), reverse=True)
-        postCounts[f'Sorted {postType}:'] = sortedCounts
+        postCounts[postType] = sortedCounts
 
     return postCounts
 #─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
@@ -171,39 +180,47 @@ def writeFiles(allPosts, postCounts, user, userDir):
 #─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 #=============================================================================================================================
 #─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
-def makeBox(list): #Takes 1 d list of strings, returns str in a box that fits.
-    fixedTabsList = [i.replace('\t', '    ') for i in list]
-    longestStr = list.count('\t')  + len(max(fixedTabsList, key=len))
-
-    box = str('─') * (longestStr) #Box drawing char, not dash.
-    boxedStr = '\n'.join([f'│{i}{ str(" " * (longestStr - len(i))) }│' for i in fixedTabsList])
-
-    return f'┌{box}┐\n{boxedStr}\n└{box}┘' #(U+2518) and (U+2510) box drawing chars respecitvely.
-#─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
-#=============================================================================================================================
-#─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 def printTotals(totalsDict): #Printed stuff after the pushshift log.
-    for k, v in totalsDict['postCounts'].items():
-        postTypeList = [k]
+    print('\n\n')
+    console = Console()
 
-        for subreddit in v:
-            new = f'\t{subreddit[0]} {subreddit[1]}'
-            postTypeList.append(new)
+    header = [
+                'Comments count',
+                f'[bold red]{totalsDict["commentsLen"]}[/bold red]',
+                'Submissions count',
+                f'[bold red]{totalsDict["submissionsLen"]}[/bold red]',
+                'Run Time'
+    ]
 
-        print(makeBox(postTypeList))
+    console.log(f'[magenta]{totalsDict["user"]}')
 
-    #Box containing # of comments/submissions, dir and path of files, and run time.
-    comments = f'Comments\t= {totalsDict["commentsLenPrint"]}'
-    submissions = f'Submissions = {totalsDict["submissionsLenPrint"]}'
-    dir = totalsDict['dir']
-    allPosts = f'\tall_posts.json'
-    sortedSubreddits = f'\tsorted_subreddits.txt'  # Could probably use a better filename.
-    runTime = f'Run time - {round(totalsDict["end"] - totalsDict["start"], 1)} s'
+    table = Table(show_header=True,header_style="bold blue")
+
+    for i in header:
+        table.add_column(i, justify='left')
+
+    commentsColumn = '\n'.join([sub[0] for sub in totalsDict['postCounts']['comments']])
+    commentsCt = '\n'.join([f'[bold red]{sub[1]}[/bold red]' for sub in totalsDict['postCounts']['comments']])
+
+    submissionsColumn = '\n'.join([sub[0] for sub in totalsDict['postCounts']['submissions']])
+    submissionsCt = '\n'.join([f'[bold red]{sub[1]}[/bold red]' for sub in totalsDict['postCounts']['submissions']])
+
+    table.add_row(
+                    f'[purple]{commentsColumn}[/purple]',
+                    commentsCt,
+                    f'[purple]{submissionsColumn}[/purple]',
+                    submissionsCt,
+                    f'[magenta underline]{round(totalsDict["end"] - totalsDict["start"], 1)} s'
+    )
 
 
-    infoToBox = [comments, submissions, '', dir, allPosts, sortedSubreddits, '', runTime]
+    #style = "white on black"
+    console.print(table, justify='center', style='bold white')
 
-    print(makeBox(infoToBox))
+    print()
+    console.print(f'\n\nArchive of reddit user {totalsDict["user"]}:', style="#af00ff")
+    console.print(f'[underline magenta]dir = {totalsDict["dir"]}')
+    console.print(f'[bold red]\tall_posts.json\n\tsubreddit_count.txt[/bold red]')
 #─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 #=============================================================================================================================
 #─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
@@ -290,19 +307,20 @@ def run(user, options):
     print(f'Gathering and formatting data from pushshift for {user}.\n')
 
     if '-pics' in options:
+        console = Console()
         keyType = {'submission': ('url', 'created_utc',)}
         images = getPosts(user, keyType, True)
 
         images = open( os.path.join(userDir,'images.txt')).read().splitlines()
         images = set(images)
-        boxedImages = [f'Images submitted by {user}'] + [f'{i+1}\t{image}' for i, image in enumerate(images)]
-        print(makeBox(boxedImages))
+        boxedImages = f'[bold blue]Images submitted by {user}\n' + '\n'.join([f'[red]{i+1}\t[magenta]{image}' for i, image in enumerate(images)])
+        console.print(boxedImages)
 
         if '-d' in options:
             print()
             imagesdl(images, userDir)
 
-        print(f'\nRun time - {round(time.time() - start, 1)} s')
+        console.print(f'\n[bold blue]Run time - {round(time.time() - start, 1)} s')
 
     else:
         keyType = {'comment': ('id', 'created_utc', 'subreddit', 'body', 'score', 'permalink', 'link_id', 'parent_id'),
@@ -313,11 +331,12 @@ def run(user, options):
         writeFiles(allPosts, postCounts, user, userDir)
 
         totalsDict = {'postCounts': postCounts,
-                      'commentsLenPrint': str( len(allPosts['comments']) ),
-                      'submissionsLenPrint': str( len(allPosts['submissions']) ),
+                      'commentsLen': str( len(allPosts['comments']) ),
+                      'submissionsLen': str( len(allPosts['submissions']) ),
                       'dir': userDir,
                       'start': start,
-                      'end': time.time()}
+                      'end': time.time(),
+                      'user':user}
 
         printTotals(totalsDict)
 #─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
