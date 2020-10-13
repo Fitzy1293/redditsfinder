a9175469a91506fcf3e7fa060c3eaceaef07c652
fitzy1293 <omfitzgerald61298@gmail.com>
2020-10-13 12:47:29 -0400

    Python file

--- using_in_python.py
+++ using_in_python.py
@@ -4,15 +4,15 @@ import redditsfinder
 from pprint import pprint
 
 # Pretty prints all submission dicts
-pprint(redditsfinder.submissions(user='spez', log=True))
+#pprint(redditsfinder.submissions(user='spez', log=True))
 
 # Pretty prints all comment bodies and text based self-posts.
-print("Comments")
-comments = redditsfinder.comments(lim=25, user='spez')
+comments = redditsfinder.comments(lim=5, user='spez')
 bodies = [v for dict in comments  for k, v in dict.items() if k == 'body']
-pprint(bodies)
 
-submissions = redditsfinder.submissions(lim=25, user='spez')
-print("\nText Posts")
-selftexts = [v for dict in submissions  for k, v in dict.items() if k == 'spez']
+submissions = redditsfinder.submissions(lim=5, user='spez')
+selftexts = [v for dict in submissions  for k, v in dict.items() if k == 'selftext']
+
+pprint(bodies)
 pprint(selftexts)
+print(len(bodies), len(selftexts))
